ce21dded431f4676ee6d6d854a0490f52711ca60
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2009-07-01 10:32:24 -0300

    First version of app's setup.

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "ynspector",
+    version = "0.1.0",
+    packages = ["ynspector"],
+    package_dir = {"ynspector": "src"},
+    author = "Guilherme Chapiewski",
+    author_email = "guilherme.chapiewski@gmail.com",
+    description = "ynspector inspects a given directory and runs a command everytime a file is changed.",
+    license = "Apache License 2.0",
+    keywords = "tool testing directory",
+    url = "http://guilhermechapiewski.github.com/ynspector/",
+    long_description = "ynspector inspects a given directory and runs a command everytime a file is changed.",
+    
+    # generate script automatically
+    entry_points = {
+        'console_scripts': [
+            'ynspect = ynspector:run',
+        ],
+    },
+
+)
