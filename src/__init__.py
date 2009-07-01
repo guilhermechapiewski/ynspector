aee3427c796195285965d967b7b0926bc7dce9d3
Guilherme Chapiewski <guilherme.chapiewski@gmail.com>
2009-07-01 11:34:43 -0300

    Input validation.

--- src/__init__.py
+++ src/__init__.py
@@ -10,5 +10,8 @@ def run():
         msg = "ynspector v%s" % version
         CLI().info_and_exit(msg)
 
+    if not options.full_path_to_dir or not options.command_to_execute:
+        CLI().error_and_exit("You need to inform the path to inspect (-d) and the command to execute (-c)")
+
     # If CLI was correctly parsed, execute ynspector
     Main(options.full_path_to_dir, options.command_to_execute).createDaemon()
